0660478c9921cf312d1bd66ea927487a9fcb8e9b
Joel Bennett <joelkbennett@gmail.com>
2021-06-07 11:41:40 -0700

    Add app icons and update manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,37 @@ export default function Home({ projects }) {
   return (
     <Layout>
       <Head>
-        <title>{'joelkb.dev'}</title>
+        <title>{'Joel K Bennett - Recent Work'}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
           content="Portfolio of work of Web Developer Joel K Bennett"></meta>
-        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <link rel="manifest" href="/manifest.json?v=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?v=1"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=1"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=1"
+        />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg?v=1"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <ProjectList projects={projects} />
       <Section title="Worked For">
