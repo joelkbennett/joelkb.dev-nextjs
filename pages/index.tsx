37d5e222409cd658e9aefa76304a52c4147d8981
Joel Bennett <joelkbennett@gmail.com>
2021-06-07 13:17:51 -0700

    Fix json formatting in manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,34 +11,34 @@ export default function Home({ projects }) {
     <Layout>
       <Head>
         <title>{'Joel K Bennett - Recent Work'}</title>
+        <link rel="manifest" href="/manifest.json?v=2" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
           content="Portfolio of work of Web Developer Joel K Bennett"></meta>
-        <link rel="manifest" href="/manifest.json?v=1" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png?v=1"
+          href="/apple-touch-icon.png?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png?v=1"
+          href="/favicon-32x32.png?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png?v=1"
+          href="/favicon-16x16.png?v=2"
         />
         <link
           rel="mask-icon"
-          href="/safari-pinned-tab.svg?v=1"
+          href="/safari-pinned-tab.svg?v=2"
           color="#5bbad5"
         />
-        <link rel="shortcut icon" href="/favicon.ico?v=1" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
