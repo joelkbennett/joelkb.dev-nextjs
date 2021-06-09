2d51b5f75217ca5291f09debaafa81d139eafeae
Joel Bennett <joelkbennett@gmail.com>
2021-06-09 11:06:50 -0700

    Change placholder softdelete from group to project

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -14,6 +14,6 @@ const prisma = global.prisma || new PrismaClient();
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
 
 // Register middleware
-softDeleteFilter(prisma, 'Group');
+softDeleteFilter(prisma, 'Project');
 
 export default prisma;
